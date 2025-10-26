e0c6a40573bd8124393f6eaf98b37cf9d1a59627
Nicolapsq <nicolapasqua84@gmail.com>
2025-10-26 18:31:27 +0100

    ex-js-hello-typescript

---
+++ Bonus/src/index.ts
@@ -0,0 +1,5 @@
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <h1>
+  Hello TypeScript
+  </h1>
+`;
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <h1>
+  Hello TypeScript
+  </h1>
+`
